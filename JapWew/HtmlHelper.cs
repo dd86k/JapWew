6bcf9350ebfe2cf397c5c18e216f195a9d3a47cc
DD~! <devddstuff@gmail.com>
2016-08-31 00:37:26 -0400

    Progress

--- JapWew/HtmlHelper.cs
+++ JapWew/HtmlHelper.cs
@@ -25,7 +25,7 @@ namespace JapWew
 
         public static string GetHtmlHexCode(char c) => $"&#x{(int)c:X2};";
 
-        public unsafe static string GetHtmlHexCode(string c) => $"&#x{GetDecimal(c):X2};";
+        public static string GetHtmlHexCode(string c) => $"&#x{GetDecimal(c):X2};";
 
         public unsafe static string GetUtf16leData(char c)
         {
@@ -60,13 +60,117 @@ namespace JapWew
             return (uint)code;
         }
 
+        //TODO: Reverse LookupEntity(string)
+        // trim '&' and ';' -> Switch
+
         //TODO: Add HTML 4.0 entities. e.g. &amp;
         // http://ascii-code.com/html-symbol.php
         public static string GetHtmlEntity(char c)
         {
-            switch (c)
+            switch ((ushort)c)
             {
+                case '"': return "&quot;";
                 case '&': return "&amp;";
+                case '<': return "&lt;";
+                case '>': return "&gt;";
+
+                case '€': return "&euro;";
+                case '¢': return "&cent;";
+                case '¤': return "&curren;";
+                case '¥': return "&yen;";
+
+                case 0x2002: return "&ensp;";
+                case 0x2003: return "&emsp;";
+                case 0x2009: return "&thinsp;";
+                case 0x200C: return "&zwnj;";
+                case 0x200D: return "&zwj;";
+                case 0x200E: return "&lrm;";
+                case 0x200F: return "&rlm;";
+                case '–': return "&ndash;";
+                case '—': return "&mdash;";
+                case '‘': return "&lsquo;";
+                case '’': return "&rsquo;";
+                case '‚': return "&sbquo;";
+                case '“': return "&ldquo;";
+                case '”': return "&rdquo;";
+                case '„': return "&bdquo;";
+                case '†': return "&dagger;";
+                case '‡': return "&Dagger;";
+                case '•': return "&bull;";
+                case '…': return "&hellip;";
+                case '‰': return "&permil;";
+                case '′': return "&prime;";
+                case '″': return "&Prime;";
+                case '‹': return "&lsaquo;";
+                case '›': return "&rsaquo;";
+                case '‾': return "&oline;";
+                case '⁄': return "&frasl;";
+
+                case '◊': return "&loz;";
+
+                case 'Α': return "&Alpha;";
+                case 'Β': return "&Beta;";
+                case 'Γ': return "&Gamma;";
+                case 'Δ': return "&Delta;";
+                case 'Ε': return "&Epsilon;";
+                case 'Ζ': return "&Zeta;";
+                case 'Η': return "&Eta;";
+                case 'Θ': return "&Theta;";
+                case 'Ι': return "&Iota;";
+                case 'Κ': return "&Kappa;";
+                case 'Λ': return "&Lambda;";
+                case 'Μ': return "&Mu;";
+                case 'Ν': return "&Nu;";
+                case 'Ξ': return "&Xi;";
+                case 'Ο': return "&Omicron;";
+                case 'Π': return "&Pi;";
+                case 'Ρ': return "&Rho;";
+                case 'Σ': return "&Sigma;";
+                case 'Τ': return "&Tau;";
+                case 'Υ': return "&Upsilon;";
+                case 'Φ': return "&Phi;";
+                case 'Χ': return "&Chi;";
+                case 'Ψ': return "&Psi;";
+                case 'Ω': return "&Omega;";
+                case 'α': return "&alpha;";
+                case 'β': return "&beta;";
+                case 'γ': return "&gamma;";
+                case 'δ': return "&delta;";
+                case 'ε': return "&epsilon;";
+                case 'ζ': return "&zeta;";
+                case 'η': return "&eta;";
+                case 'θ': return "&theta;";
+                case 'ι': return "&iota;";
+                case 'κ': return "&kappa;";
+                case 'λ': return "&lambda;";
+                case 'μ': return "&mu;";
+                case 'ν': return "&nu;";
+                case 'ξ': return "&xi;";
+                case 'ο': return "&omicron;";
+                case 'π': return "&pi;";
+                case 'ρ': return "&rho;";
+                case 'ς': return "&sigmaf;";
+                case 'σ': return "&sigma;";
+                case 'τ': return "&tau;";
+                case 'υ': return "&upsilon;";
+                case 'φ': return "&phi;";
+                case 'χ': return "&chi;";
+                case 'ψ': return "&psi;";
+                case 'ω': return "&omega;";
+                case 'ϑ': return "&thetasym;";
+                case 'ϒ': return "&upsih;";
+                case 'ϖ': return "&piv;";
+
+                case '←': return "&larr;";
+                case '↑': return "&uarr;";
+                case '→': return "&rarr;";
+                case '↓': return "&rarr;";
+                case '↔': return "&harr;";
+                case '⇐': return "&lArr;";
+                case '⇑': return "&uArr;";
+                case '⇒': return "&rArr;";
+                case '⇓': return "&dArr;";
+                case '⇔': return "&hArr;";
                 default:  return null;
             }
         }
---
+++ JapWew/LookupPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//HtmlHelper.LookupEntity()
+
+namespace JapWew
+{
+    /// <summary>
+    /// Interaction logic for LookupPage.xaml
+    /// </summary>
+    public partial class LookupPage : Page
+    {
+        public LookupPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
